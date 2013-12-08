1d4af510201ecc06c24cd2d87e3a58f97f641b6a
KaiGeffen <geffenkai@gmail.com>
2013-12-08 14:04:39 -0500

    Turns grouped by alignment

--- script/time/addNext.py
+++ script/time/addNext.py
@@ -3,7 +3,39 @@
 from bge import logic
 
 def unitAction(unit, timeArray):
-	if unit['speed'] > 0:
-		firstTurn = 100 - unit['speed']
+	# If unit has no/negative speed, it cannot act
+	if unit['speed'] <= 0:
+		return
 
-		timeArray[firstTurn].append(unit)
+	# The turn number of the first turn in which unit acts
+	turnNumber = 100 - unit['speed']
+	turn = timeArray[turnNumber]
+
+	# Turns are divided into each alignment
+	# Add unit to the group that matchs its alignment
+	turn = addUnitToAlignGroup(unit, turn)
+
+	timeArray.append(turn)
+
+	print(timeArray)
+
+
+# Add unit to the group within turn that matchs unit's alignment
+def addUnitToAlignGroup(unit, turn):
+		# Find group with same align as unit and add unit to that group
+		# If group doesn't yet exist, add new group below
+		for i in range(0, len(turn)):
+
+			group = turn[i]
+			# Get first unit because there will be at least 1 and all grouped unit share align
+			groupAlign = group[0]['align']
+
+			if groupAlign == unit['align']:
+				# Return turn with the unit added to correct group
+				return turn[i].append(unit)
+
+		# If turn was not returned in above, group must not yet exist
+		newGroup = [unit]
+		return turn.append(newGroup)
+
+
