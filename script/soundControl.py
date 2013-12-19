5a7e7ce28cd532805434523d13cf87194f20989c
KaiGeffen <geffenkai@gmail.com>
2013-12-19 15:23:49 -0500

    Simplified characters, made change to audio which should better handle importing sound files. Bug was occasionally coming up where all loaded resources got wiped, presumably because sound files weren't being handled correctly.

--- script/soundControl.py
+++ script/soundControl.py
@@ -3,12 +3,23 @@ from bge import logic
 import aud
 
 device = aud.device()
+storedSounds = {}
 
 # Play a sound with given name once
 # Sound wav file must exist in audio
 def play(soundName):
+	# Determine if a factory for sound already exists
+	soundAlreadyStored = soundName in storedSounds
+
+	if not soundAlreadyStored:
+		storeSound(soundName)
+
+	device.play(storedSounds[soundName])
+
+# Store in storedSounds a pairing of the sound's name and its aud.Factory
+def storeSound(soundName):
+	# Make an aud.factory for sound
 	filepath = logic.expandPath('//audio/') + soundName + '.wav'
+	soundFactory = aud.Factory(filepath)
 
-	sound = aud.Factory(filepath)
-
-	device.play(sound)
+	storedSounds[soundName] = soundFactory
