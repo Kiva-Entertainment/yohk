e5029cb530bf72a067049c3bf3fda6f02b00a494
KaiGeffen <geffenkai@gmail.com>
2013-12-25 17:07:10 -0500

    Made very hacky 'correct' turn order display

--- script/time/displayTurnOrder.py
+++ script/time/displayTurnOrder.py
@@ -8,20 +8,24 @@ OVERLAY_SCENE_NAME = 'battlefieldOverlay'
 # Name of the text object which displays turn order
 TURN_ORDER_DISPLAY= 'displayTurnOrder'
 
+MAX_LINES = 30
+
 def do():
-	text = formDisplayedText()
+	timeline = expectedTimeLine()
+	text = formTimelineText(timeline)
 	
 	# Set the text of the object that displays turn order
 	turnDisplay = objectControl.getFromScene(TURN_ORDER_DISPLAY, OVERLAY_SCENE_NAME)
 	turnDisplay['Text'] = text
 
+# The timeline that is expected if no units die/have their speed changed
+def expectedTimeLine():
+	expectedTime = []
+	for i in range(101):
+		expectedTime.append([])
 
-# For each turn with actors, add all of those actors, followed by
-# a blank line to communicate which units act on same turn
-def formDisplayedText():
-	text = ''
-	
 	time = logic.globalDict['time']
+
 	for turnNum in range(0, len(time)):
 		turn = time[turnNum]
 
@@ -32,15 +36,40 @@ def formDisplayedText():
 				group = turn[groupNum]
 
 				for actor in group:
+					name = actor['name']
 
-					# Start with turn number
-					text += str(turnNum)
-					# Add the group number
-					text += '.' + str(groupNum) + ': '
-					# End line with actor's name
-					text += actor['name'] + '\n'
-			
-			# Follow (turn with actors) with a blank line
-			text += '\n'
+					expectedTime[turnNum].append(name)
+
+					if actor['speed'] > 0:
+						nextTurnNum = turnNum + round(100/actor['speed'])
+
+						while nextTurnNum < 100:
+							expectedTime[nextTurnNum].append(name)
+							nextTurnNum += round(100/actor['speed'])
+	return expectedTime
+
+# For each turn with actors, add all of those actors, followed by
+# a blank line to communicate which units act on same turn
+def formTimelineText(timeline):
+	text = ''
 	
+	# Form full text
+	for turn in timeline:
+
+		if turn != []:
+			for unit in turn:
+				text += unit + '\n'
+			text += '\n'
+
+	# Crop to fit max lines
+	# NOTE(kgeffen) Even last line has a trailing newline
+	if text.count('\n') > MAX_LINES:
+
+		fittingText = ''
+		textLines = text.split('\n')
+		for i in range(MAX_LINES):
+			fittingText += textLines[i] + '\n'
+
+		return fittingText
+
 	return text
